a354dcdb9e5bbd98613c945a006c8bbb04a39b5e
Messias.Xavier <messias.xavier@sicoob.com.br>
2019-02-25 20:48:45 -0300

    improving form category

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -6,6 +6,8 @@ import { Category } from '../shared/category.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
+import toastr from "toastr";
+
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -35,6 +37,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   ngAfterContentChecked() {
     this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+    if (this.currentAction == 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
   }
 
   //PRIVATE METHODS
@@ -77,6 +88,41 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     } else {
       const categoryName = this.category.name || '';
       this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory(): any {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+
+    )
+  }
+
+  private updateCategory(): any {
+
+  }
+
+  private actionsForSuccess(category: Category): void {
+    toastr.success('Solicitação processada com sucesso!');
+
+    //redirect/reload component page
+    this.router.navigateByUrl('categories', { skipLocationChange: true }).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error: any): void {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor tente mais tarde.']
     }
   }
 }

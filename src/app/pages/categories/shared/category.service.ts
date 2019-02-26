adc0dab4e2d4704097eae23b23c208e02f7d8a2f
messias <messiasxavier@gmail.com>
2019-02-25 23:19:01 -0300

    completing the category module

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -40,7 +40,7 @@ export class CategoryService {
   update(category: Category): Observable<Category> {
     const url = `${this.apiPath}/${category.id}`;
 
-    return this.http.put(url, category).pipe(
+    return this.http.put('url', category).pipe(
       catchError(this.handleError),
       map(() => category)
     )

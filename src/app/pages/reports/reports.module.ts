f708767bdf1e0cd1bac0349c4777e058ffdee3d2
Messias.Xavier <messias.xavier@sicoob.com.br>
2019-02-28 11:29:19 -0300

    improving Reports

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { ChartModule } from "primeng/chart";
+
+
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
     SharedModule,
-    ReportsRoutingModule
+    ReportsRoutingModule,
+    ChartModule
   ]
 })
 export class ReportsModule { }
--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Category } from '../../categories/shared/category.model';
+import { EntryService } from '../../entries/shared/entry.service';
+import { CategoryService } from '../../categories/shared/category.service';
+import { Entry } from '../../entries/shared/entry.model';
+
+import currencyFormatter from "currency-formatter";
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +13,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  reveneuTotal: any = 0;
+  balance: any = 0;
 
-  ngOnInit() {
+  expenseChartData: any;
+  reveneuChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
   }
 
+  categories: Category[] = [];
+  entries: Entry[] = [];
+
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
+
+  constructor(private entryService: EntryService, private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.categoryService.getAll()
+      .subscribe(categories => this.categories = categories);
+  }
+
+  generateReports() {
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
+
+    if (!month || !year) {
+      alert('Você precisa selecionar o Mês e o Ano para gerar os relatórios')
+    } else {
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this));
+    }
+  }
+
+  private setValues(entries: Entry[]) {
+    this.entries = entries;
+    this.calculateBalance();
+    this.setChartData();
+  }
+
+  private calculateBalance(): void {
+    let expenseTotal = 0;
+    let reveneuTotal = 0;
+
+    this.entries.forEach(entry => {
+      if (entry.type == 'reveneu') {
+        reveneuTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
+      } else {
+        expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
+      }
+    });
+
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL' });
+    this.reveneuTotal = currencyFormatter.format(reveneuTotal, { code: 'BRL' });
+    this.balance = currencyFormatter.format(reveneuTotal - expenseTotal, { code: 'BRL' });
+  }
+
+  private setChartData() {
+    this.reveneuChartData = this.getChartData('reveneu', 'Gráfico de Receitas', '#9CCC65');
+    this.expenseChartData = this.getChartData('expense', 'Gráfico de Despesas', '#e03131');
+  }
+
+  private getChartData(entryType: string, title: string, color: string) {
+    const chartData = [];
+
+    this.categories.forEach(category => {
+      //filtering entries by category and type
+      const filteredEntries = this.entries.filter(
+        entry => (entry.categoryId == category.id) && (entry.type == entryType)
+      );
+
+      //if found entries, then entries amount and add to chartData
+      if (filteredEntries.length > 0) {
+        const totalAmount = filteredEntries.reduce(
+          (total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BRL' }), 0
+        )
+
+        chartData.push({
+          categoryName: category.name,
+          totalAmount: totalAmount
+        })
+      }
+    });
+
+    return {
+      labels: chartData.map(item => item.categoryName),
+      datasets: [{
+        label: title,
+        backgroundColor: color,
+        data: chartData.map(item => item.totalAmount)
+      }]
+    }
+  }
 }
